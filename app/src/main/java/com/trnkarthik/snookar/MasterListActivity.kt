74ff96c2b3b62b0a0891e43cf11732b046da55e1
Karthik Tangirala <karthik.trn@gmail.com>
2017-01-01 03:49:15 -0700

    Initial commit

---
+++ app/src/main/java/com/trnkarthik/snookar/MasterListActivity.kt
@@ -0,0 +1,11 @@
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.trnkarthik.snookar.R
+
+class MasterListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_master_list)
+    }
+}
